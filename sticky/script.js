d191f7818add024eeea07166222f142513b92cc7
wayne <waynekim07@gmail.com>
2025-05-25 10:53:30 +0300

    Added the drag functionality to the sticky notes

--- sticky/script.js
+++ sticky/script.js
@@ -2,8 +2,28 @@ let z = 1
 const stickyNotes = document.querySelectorAll(".sticky-note")
 
 stickyNotes.forEach(stickyNote=>{
-    stickyNote.addEventListener("click",(e)=>{
+    stickyNote.addEventListener("mousedown",(e)=>{
         z+=1
         stickyNote.style.zIndex = z
+
+        const header = stickyNote.querySelector(".header")
+        header.addEventListener("mousedown",event=>{
+            const posLeft = stickyNote.offsetLeft
+            const posTop = stickyNote.offsetTop
+            
+            const startX  = event.pageX
+            const startY  = event.pageY
+
+            const drag = (event)=>{
+                stickyNote.style.left = `${posLeft + (event.pageX - startX)}px`
+                stickyNote.style.top = `${posTop + (event.pageY - startY)}px`
+            }
+            const mouseUp = ()=>{
+                document.removeEventListener("mousemove",drag)
+                document.removeEventListener("mouseup",mouseUp)
+            }
+            document.addEventListener("mousemove",drag)
+            document.addEventListener("mouseup",mouseUp)
+        })
     })
 })  
